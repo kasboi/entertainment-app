f79d893f80bacd60b99c1d6b95c68e4991ba24fb
Abdullah <solahudeen15@gmail.com>
2024-11-20 09:34:58 +0100

    fav pages grid fix

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -30,7 +30,7 @@ export default function Page() {
   }
 
   return (
-    <div className="py-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 max-w-screen-lg mx-auto custom-scrollbar rounded-lg">
+    <div className="py-4 grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-6 max-w-screen-lg mx-auto custom-scrollbar rounded-lg">
       {movies.map((movie) => (
         <MovieCard
           key={movie.id}
